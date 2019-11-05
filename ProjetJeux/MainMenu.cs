aa658a79c1b005d45aae0781f8b530764cbd0866
David FLEMAL <David.FLEMAL@student.hel.be>
2019-11-05 14:35:08 +0100

    Add functions EnableButtons and DisableButtons

--- ProjetJeux/MainMenu.cs
+++ ProjetJeux/MainMenu.cs
@@ -19,17 +19,26 @@ namespace ProjetJeux
 
         private void BtnStart_Click(object sender, EventArgs e)
         {
-
+            EnableButtons();
         }
 
         private void BtnLoad_Click(object sender, EventArgs e)
         {
-
+            EnableButtons();
         }
 
         private void BtnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void EnableButtons()
+        {
+            BtnStart.Enabled = BtnLoad.Enabled = BtnExit.Enabled = BtnStart.Visible = BtnLoad.Visible = BtnExit.Visible = false;
+        }
+        private void DisableButtons()
+        {
+            BtnStart.Enabled = BtnLoad.Enabled = BtnExit.Enabled = BtnStart.Visible = BtnLoad.Visible = BtnExit.Visible = true;
         }
     }
 }
